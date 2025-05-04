b656ec64aa1390bd49f57d5fdbf815f987ae6e4e
Eduardo Ribeiro <eduardo.ribeiro@gtplan.com.br>
2025-05-04 09:47:21 -0300

    🔧 config: Configurando o cliente Prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,13 +1,13 @@
-import { PrismaClient } from "@prisma/client";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import { PrismaClient } from "../../prisma/generated/prisma";
 
-/**
- * Cliente Prisma singleton para uso em toda a aplicação
- * @see https://www.prisma.io/docs/guides/other/troubleshooting-orm/help-articles/nextjs-prisma-client-dev-practices
- */
-const globalForPrisma = globalThis as unknown as {
-	prisma: PrismaClient | undefined;
+const globalForPrisma = global as unknown as {
+	prisma: PrismaClient;
 };
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+const prisma =
+	globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate());
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+export default prisma;
